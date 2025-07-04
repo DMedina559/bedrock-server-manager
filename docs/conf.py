1c7fb405fb6d1572943c577a14a9c0fb368e2ae7
Danny <68480549+DMedina559@users.noreply.github.com>
2025-07-04 09:25:22 -0700

    docs: better styling

--- docs/conf.py
+++ docs/conf.py
@@ -37,4 +37,5 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_favicon = "_static/favicon.ico"
 html_logo = "_static/favicon-96x96.png"
+html_css_files = ["css/custom_sphinx_styles.css"]
 
