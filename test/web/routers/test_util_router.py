50313143d29a1e125ac9ccee650a1f10af0588f8
Danny <68480549+DMedina559@users.noreply.github.com>
2025-08-18 08:43:26 -0700

    test: no patch needed

--- test/web/routers/test_util_router.py
+++ test/web/routers/test_util_router.py
@@ -106,10 +106,6 @@ def test_serve_world_icon_api_not_found(
     assert response.status_code == 404
 
 
-@patch(
-    "bedrock_server_manager.config.bcm_config.needs_setup",
-    new=lambda app_context: False,
-)
 @patch("bedrock_server_manager.web.routers.util.os.path.exists")
 def test_get_root_favicon_not_found(mock_exists, client: TestClient):
     """Test the get_root_favicon route with no favicon found."""
