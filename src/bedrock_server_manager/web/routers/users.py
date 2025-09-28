80e7a1ba0dbc5a03d953a1dc1c7eca42899f8063
Danny <68480549+DMedina559@users.noreply.github.com>
2025-09-28 14:25:08 -0700

    style: black format

--- src/bedrock_server_manager/web/routers/users.py
+++ src/bedrock_server_manager/web/routers/users.py
@@ -9,7 +9,12 @@ from fastapi.templating import Jinja2Templates
 
 from ...db.models import User
 from ..dependencies import get_templates, get_app_context
-from ..auth_utils import get_current_user, get_password_hash, get_admin_user, get_moderator_user
+from ..auth_utils import (
+    get_current_user,
+    get_password_hash,
+    get_admin_user,
+    get_moderator_user,
+)
 from ..schemas import User as UserSchema
 from .audit_log import create_audit_log
 from ...context import AppContext
