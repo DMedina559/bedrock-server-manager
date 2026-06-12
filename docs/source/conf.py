e524e1a403582b3b707f4ac0ef95e268ae23fa47
Danny <68480549+DMedina559@users.noreply.github.com>
2026-06-12 12:36:21 -0700

    docs: fix changelog gen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,49 +52,17 @@ sphinx_github_changelog_token = os.environ.get(
 ) or os.environ.get("GITHUB_TOKEN")
 
 # MonkeyPatch sphinx_github_changelog to ignore pre-releases
-original_extract_releases = sphinx_github_changelog.changelog.extract_releases
-
-
-def extract_releases_with_prerelease(owner_repo, token, graphql_url=None):
-    # Overriding to include isPrerelease in the query
-    from sphinx_github_changelog.changelog import ChangelogError, github_call
-
-    owner, repo = owner_repo.split("/")
-    query = f"""
-    query {{
-        repository(owner: "{owner}", name: "{repo}") {{
-            releases(orderBy: {{field: CREATED_AT, direction: DESC}}, first:100) {{
-                nodes {{
-                    name, description, descriptionHTML, url, tagName, publishedAt, isDraft, isPrerelease
-                }}
-            }}
-        }}
-    }}
-    """
-    full_query = {"query": query.replace("\n", "")}
-
-    url = "https://api.github.com/graphql" if graphql_url is None else graphql_url
-
-    result = github_call(url=url, query=full_query, token=token)
-    if "errors" in result:
-        raise ChangelogError(
-            "GitHub API error response: \n"
-            + "\n".join(e.get("message", str(e)) for e in result["errors"])
-        )
-    try:
-        releases = result["data"]["repository"]["releases"]["nodes"]
-        return [r for r in releases if r]
-    except (KeyError, TypeError):
-        raise ChangelogError(f"GitHub API error unexpected format:\n{result!r}")
-
-
-sphinx_github_changelog.changelog.extract_releases = extract_releases_with_prerelease
-
 original_node_for_release = sphinx_github_changelog.changelog.node_for_release
 
 
 def node_for_release_ignore_prerelease(release, pypi_name=None):
-    if release.get("isPrerelease", False):
+    is_pre = False
+    if isinstance(release, dict):
+        is_pre = release.get("isPrerelease", False) or release.get("prerelease", False)
+    else:
+        is_pre = getattr(release, "is_prerelease", False)
+
+    if is_pre:
         return None
     return original_node_for_release(release, pypi_name)
 
